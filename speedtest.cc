a5ba936f33d6df2c0cc21802fb66e3eca22bc851
Juha Nieminen <warp@iki.fi>
2009-10-20 21:34:40 +0300

    Added option -html to the speedtest program.

--- speedtest.cc
+++ speedtest.cc
@@ -7,17 +7,17 @@
 #include <iostream>
 #include <cmath>
 #include <sstream>
+#include <cstring>
 
 #include <sys/time.h>
 
 //#define FUNC0 x+y+(sin(x)*cos(x)*log(x)*(-x-y+log(y)-sin(x)))
 //#define FUNC0 pow(x,14)+pow(y,8)+pow(x,2)+2*x*y+pow(y,2)
 #define FUNC0 pow(x,14)+pow(y,8)
-#define FUNC1 ((3*pow(x,4)-7*pow(x,3)+2*x*x-4*x+10)-(4*pow(y,3)+2*y*y-10*y+2))*10
-
+#define FUNC1 ((3*pow(x,4)-7*pow(x,3)+2*x*x-4*x+10) - (4*pow(y,3)+2*y*y-10*y+2))*10
 #define FUNC2 ((3*(x+(5*(y+2)-7*x)*3-y)+4*5+3)-7+(8*x+5*y+(7-x))*4)-10*3+4
 #define FUNC3 sin(sqrt(10-x*x+y*y))+cos(sqrt(5-x*x-y*y))+sin(x*x+y*y)
-#define FUNC4 exp((-x*x-y*y)/100)*sin(sqrt(x*x+y*y))/(10*2)+sin(pow(x,4)-4*pow(x,3)+3*x*x-2*x+2*5-3)-cos(-2*pow(y,4)+5*pow(y,3)-14*x*x+8*x-120/2+4)
+#define FUNC4 exp((-x*x-y*y)/100)*sin(sqrt(x*x+y*y))/(10*2) + sin(pow(x,4)-4*pow(x,3)+3*x*x-2*x+2*5-3) - cos(-2*pow(y,4)+5*pow(y,3)-14*x*x+8*x-120/2+4)
 
 #define StringifyHlp(x) #x
 #define Stringify(x) StringifyHlp(x)
@@ -31,6 +31,8 @@ double funcName(const double* vars) \
 
 namespace
 {
+    bool gPrintHTML = false;
+
     struct FuncData
     {
         const char* const funcStr;
@@ -61,7 +63,7 @@ namespace
         os << value;
         std::string result = os.str();
         for(size_t i = result.size(); i > 3;)
-            result.insert(i -= 3, 1, ' ');
+            result.insert(i -= 3, gPrintHTML ? "&nbsp;" : " ");
         return result;
     }
 }
@@ -91,8 +93,13 @@ public:
     }
     void Report(const char* title, const char* unit)
     {
-        std::cout << title << ": "
-                  << (result) << " us. ("
+        if(gPrintHTML)
+        {
+            std::cout.precision(2);
+            std::cout << " <li>";
+        }
+        std::cout << title << ": " << result
+                  << (gPrintHTML ? " &micro;s. (" : " us. (")
                   << beautify(int(1e6/result)) << " " << unit << "/s)\n";
     }
     void TakeResult()
@@ -117,8 +124,10 @@ private:
     double result;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
+    gPrintHTML = argc > 1 && std::strcmp(argv[1], "-html") == 0;
+
     FunctionParser fp, fp2;
     double values[3] = { 1, 2, 3 };
 
@@ -126,8 +135,12 @@ int main()
     {
         // Parse function
         // --------------
-        std::cout << "\n--- Function:\n\"" << funcData[i].funcStr
-                  << "\"" << std::endl;
+        if(gPrintHTML)
+            std::cout << "\n<hr>\n<p>Function:\n<code>\""
+                      << funcData[i].funcStr << "\"</code>" << std::endl;
+        else
+            std::cout << "\n--- Function:\n\"" << funcData[i].funcStr
+                      << "\"" << std::endl;
 
         int res = fp.Parse(funcData[i].funcStr, funcData[i].paramStr);
         if(res >= 0)
@@ -143,6 +156,7 @@ int main()
 
         Test tester;
 
+        if(gPrintHTML) std::cout << "<ul>\n";
 
         // Measure parsing speed
         // ---------------------
@@ -200,12 +214,17 @@ int main()
 
         // Measure C++ function speed
         // --------------------------
-        tester.Start(FuncLoops);
-        while(tester.Loop())
-            funcData[i].function(values);
+        if(!gPrintHTML)
+        {
+            tester.Start(FuncLoops);
+            while(tester.Loop())
+                funcData[i].function(values);
 
-        tester.Report("C++ function time", "evals");
+            tester.Report("C++ function time", "evals");
+        }
 #endif
+
+        if(gPrintHTML) std::cout << "</ul>\n";
     }
 
     return 0;
