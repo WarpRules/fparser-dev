fc352e69a9c777c2b8780640f706a4fdbb6f7f40
Joel Yliluoma <joel.yliluoma@iki.fi>
2023-07-29 18:49:11 +0300

    report_time: add colors

--- report_time.sh
+++ report_time.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export TIME='[38;5;171muser [38;5;223m%U[38;5;171m, system [38;5;223m%S[38;5;171m, elapsed [38;5;223m%E[38;5;171m, CPU [38;5;223m%P[38;5;171m, mem [38;5;223m%Mk[m'
 time="$(time /bin/sh -c "$* 1>&8 2>&7" 2>&1)" 8>&1 7>&2
 m=$?
 if [ ! $m = 0 ]; then
