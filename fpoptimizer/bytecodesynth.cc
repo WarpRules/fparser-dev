WarpRules/fparser-dev

commit b837f3b29251910a9ffb68cdf3e8dc671a2e4f87

Add fused-multiply-and-add (and sub) support, see long commit message for details
Added new opcodes:
	cFma = x*y + a
	cFms = x*y - a
	cFmma = x*y + a*b
	cFmms = x*y - a*b

And associated optimization rules.

Using this test expression:
	(z*(x*y+z*2)+x*(y+3))*x+(x*y+z)*4

Timings:

       ,-----------------------------------------------------------------------------,
       |                  Parse |  Eval | Eval (O) | Eval (O2) | Optimize | Repeat O.|
,------+------------------------+-------+----------+-----------+----------+----------+
|         float, before   0.871 | 0.061 |    0.062 |     0.060 |    127.0 |    123.9 |
|         float, after    0.881 | 0.054 |    0.046 |     0.047 |    134.7 |    134.5 |
|-------------------------------+-------+----------+-----------+----------+----------|
|        double, before   0.841 | 0.056 |    0.053 |     0.056 |    128.5 |    125.3 |
|        double, after    0.877 | 0.055 |    0.043 |     0.043 |    134.3 |    134.3 |
|-------------------------------+-------+----------+-----------+----------+----------|
|   long double, before   0.988 | 0.108 |    0.105 |     0.105 |    140.3 |    137.7 |
|   long double, after    0.887 | 0.094 |    0.079 |     0.079 |    137.5 |    137.5 |
|-------------------------------+-------+----------+-----------+----------+----------|
|complex double, before   0.861 | 0.100 |    0.092 |     0.092 |    111.7 |    108.5 |
|complex double, after    0.927 | 0.083 |    0.058 |     0.059 |    126.8 |    125.8 |
|-------------------------------+-------+----------+-----------+----------+----------|
|     MpfrFloat, before   5.361 | 0.963 |    1.030 |     1.023 |    170.7 |    173.1 |
|     MpfrFloat, after    5.627 | 0.967 |    0.977 |     0.973 |    194.5 |    196.0 |
'------------------------------------------------------------------------------------'

fpoptimizer/bytecodesynth.cc
@@ -32,13 +32,15 @@ namespace FPoptimizer_ByteCode
 #define mData this
 #define mByteCode ByteCode
 #define mImmed Immed
+#define mStackPtr     StackTop
+#define mStackSize    StackMax
+#define incStackPtr() ProducedNParams(1)
 
     /* Specializer<bool,bool>: The bools are IsIntType, IsComplexType. */
     template<typename Value_t>
     void ByteCodeSynth<Value_t>::AddFunctionOpcode(unsigned opcode,
                                                    Specializer<false,false>)
     {
-        int mStackPtr=0;
 # define FP_FLOAT_VERSION 1
 # define FP_COMPLEX_VERSION 0
 # include "extrasrc/fp_opcode_add.inc"
@@ -50,7 +52,6 @@ namespace FPoptimizer_ByteCode
     void ByteCodeSynth<Value_t>::AddFunctionOpcode(unsigned opcode,
                                                    Specializer<true,false>)
     {
-        int mStackPtr=0;
 # define FP_FLOAT_VERSION 0
 # define FP_COMPLEX_VERSION 0
 # include "extrasrc/fp_opcode_add.inc"
@@ -63,7 +64,6 @@ namespace FPoptimizer_ByteCode
     void ByteCodeSynth<Value_t>::AddFunctionOpcode(unsigned opcode,
                                                    Specializer<false,true>)
     {
-        int mStackPtr=0;
 # define FP_FLOAT_VERSION 1
 # define FP_COMPLEX_VERSION 1
 # include "extrasrc/fp_opcode_add.inc"
@@ -75,7 +75,6 @@ namespace FPoptimizer_ByteCode
     void ByteCodeSynth<Value_t>::AddFunctionOpcode(unsigned opcode,
                                                    Specializer<true,true>)
     {
-        int mStackPtr=0;
 # define FP_FLOAT_VERSION 0
 # define FP_COMPLEX_VERSION 1
 # include "extrasrc/fp_opcode_add.inc"
@@ -84,10 +83,55 @@ namespace FPoptimizer_ByteCode
     }
 #endif
 
+    template<typename Value_t>
+    void ByteCodeSynth<Value_t>::PushVar(unsigned varno)
+    {
+        FP_TRACE_BYTECODE_ADD_VAR(varno);
+        ByteCode.push_back(varno);
+        ProducedNParams(1);
+    }
+
+    template<typename Value_t>
+    void ByteCodeSynth<Value_t>::PushImmed(Value_t immed)
+    {
+        FP_TRACE_BYTECODE_ADD_IMMED(immed);
+        ByteCode.push_back(cImmed);
+        Immed.push_back(immed);
+        ProducedNParams(1);
+    }
+
+    template<typename Value_t>
+    void ByteCodeSynth<Value_t>::DoPopNMov(size_t targetpos, size_t srcpos)
+    {
+        FP_TRACE_BYTECODE_ADD(cPopNMov);
+        ByteCode.push_back(cPopNMov);
+        ByteCode.push_back( 0x80000000u | (unsigned) targetpos);
+        ByteCode.push_back( 0x80000000u | (unsigned) srcpos);
+
+        SetStackTop(srcpos+1);
+        StackState[targetpos] = StackState[srcpos];
+        SetStackTop(targetpos+1);
+    }
+
+    template<typename Value_t>
+    void ByteCodeSynth<Value_t>::DoDup(size_t src_pos)
+    {
+        if(src_pos == StackTop-1)
+        {
+            FP_TRACE_BYTECODE_ADD(cDup);
+            ByteCode.push_back(cDup);
+        }
+        else
+        {
+            FP_TRACE_BYTECODE_ADD(cFetch);
+            ByteCode.push_back(cFetch);
+            ByteCode.push_back( 0x80000000u | (unsigned) src_pos);
+        }
+        ProducedNParams(1);
+        StackState[StackTop-1] = StackState[src_pos];
+    }
+
 #undef findName
-#undef mImmed
-#undef mByteCode
-#undef mData
 #undef TryCompilePowi
     /*******/
 }
@@ -622,6 +666,10 @@ namespace FPoptimizer_ByteCode
 {
 #define FP_INSTANTIATE(type) \
     template struct SequenceOpcodes<type>; \
+    template void ByteCodeSynth<type>::PushVar(unsigned); \
+    template void ByteCodeSynth<type>::PushImmed(type); \
+    template void ByteCodeSynth<type>::DoPopNMov(size_t, size_t); \
+    template void ByteCodeSynth<type>::DoDup(size_t); \
     template void ByteCodeSynth<type>::AddFunctionOpcode(unsigned); \
     template void ByteCodeSynth<type>::AddFunctionOpcode(unsigned, \
                  Specializer< bool(FUNCTIONPARSERTYPES::IsIntType<type>::value), \

extrasrc/fpaux.hh
@@ -51,6 +51,8 @@ namespace FUNCTIONPARSERTYPES
     bool IsAlwaysRealOpcode(unsigned op);
     bool IsUnaryOpcode(unsigned op);
     bool IsBinaryOpcode(unsigned op);
+    bool IsTernaryOpcode(unsigned op);
+    bool IsQuartaryOpcode(unsigned op);
     bool IsVarOpcode(unsigned op);
     bool IsCommutativeOrParamSwappableBinaryOpcode(unsigned op);
     unsigned GetParamSwappedBinaryOpcode(unsigned op);
@@ -1250,6 +1252,74 @@ namespace FUNCTIONPARSERTYPES
         return fp_polar_scalar<T> (fp_exp2(x.real()), x.imag()*fp_const_log2<T>());
     }
   #endif
+#line 1 "extrasrc/functions/func_fma.hh"
+    template<typename Value_t>
+    inline Value_t fp_fma(const Value_t& x, const Value_t& y,
+                          const Value_t& a)
+    {
+        return x*y+a;
+    }
+
+  #ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fma(const MpfrFloat& x, const MpfrFloat& y,
+                            const MpfrFloat& a)
+    {
+        return MpfrFloat::fma(x,y, a);
+    }
+  #endif
+
+    // long, mpfr and complex use the default implementation.
+#line 1 "extrasrc/functions/func_fmma.hh"
+    template<typename Value_t>
+    inline Value_t fp_fmma(const Value_t& x, const Value_t& y,
+                           const Value_t& a, const Value_t& b)
+    {
+        return x*y+a*b;
+    }
+
+  #ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fmma(const MpfrFloat& x, const MpfrFloat& y,
+                             const MpfrFloat& a, const MpfrFloat& b)
+    {
+        return MpfrFloat::fmma(x,y, a,b);
+    }
+  #endif
+
+    // long, mpfr and complex use the default implementation.
+#line 1 "extrasrc/functions/func_fmms.hh"
+    template<typename Value_t>
+    inline Value_t fp_fmms(const Value_t& x, const Value_t& y,
+                           const Value_t& a, const Value_t& b)
+    {
+        return x*y-a*b;
+    }
+
+  #ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fmms(const MpfrFloat& x, const MpfrFloat& y,
+                             const MpfrFloat& a, const MpfrFloat& b)
+    {
+        return MpfrFloat::fmms(x,y, a,b);
+    }
+  #endif
+
+    // long, mpfr and complex use the default implementation.
+#line 1 "extrasrc/functions/func_fms.hh"
+    template<typename Value_t>
+    inline Value_t fp_fms(const Value_t& x, const Value_t& y,
+                          const Value_t& a)
+    {
+        return x*y-a;
+    }
+
+  #ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fms(const MpfrFloat& x, const MpfrFloat& y,
+                            const MpfrFloat& a)
+    {
+        return MpfrFloat::fms(x,y, a);
+    }
+  #endif
+
+    // long, mpfr and complex use the default implementation.
 #line 1 "extrasrc/functions/func_imag.hh"
     template<typename Value_t>
     inline Value_t fp_imag(const Value_t& ) { return Value_t{}; }
@@ -1708,7 +1778,7 @@ namespace FUNCTIONPARSERTYPES
     template<typename T>
     struct IsComplexType<std::complex<T> >: public std::true_type { };
   #endif
-#line 1712 "extrasrc/fpaux.hh"
+#line 1782 "extrasrc/fpaux.hh"
 //$PLACEMENT_END
 
 } // namespace FUNCTIONPARSERTYPES

extrasrc/fptypes.hh
@@ -84,6 +84,11 @@ namespace FUNCTIONPARSERTYPES
         cRSub,  /* reverse subtraction (not x-y, but y-x) */
         cRSqrt, /* inverse square-root (1/sqrt(x)) */
 
+        cFma,   /* Fused multiply-and-add: a*b+c */
+        cFms,   /* Fused multiply-and-sub: a*b-c */
+        cFmma,  /* Fused multiply-and-add: a*b+c*d */
+        cFmms,  /* Fused multiply-and-sub: a*b-c*d */
+
         VarBegin
     };
 

extrasrc/functions/func_fma.hh
@@ -1,0 +1,16 @@
+    template<typename Value_t>
+    inline Value_t fp_fma(const Value_t& x, const Value_t& y,
+                          const Value_t& a)
+    {
+        return x*y+a;
+    }
+
+#ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fma(const MpfrFloat& x, const MpfrFloat& y,
+                            const MpfrFloat& a)
+    {
+        return MpfrFloat::fma(x,y, a);
+    }
+#endif
+
+    // long, mpfr and complex use the default implementation.

extrasrc/functions/func_fmma.hh
@@ -1,0 +1,16 @@
+    template<typename Value_t>
+    inline Value_t fp_fmma(const Value_t& x, const Value_t& y,
+                           const Value_t& a, const Value_t& b)
+    {
+        return x*y+a*b;
+    }
+
+#ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fmma(const MpfrFloat& x, const MpfrFloat& y,
+                             const MpfrFloat& a, const MpfrFloat& b)
+    {
+        return MpfrFloat::fmma(x,y, a,b);
+    }
+#endif
+
+    // long, mpfr and complex use the default implementation.

extrasrc/functions/func_fmms.hh
@@ -1,0 +1,16 @@
+    template<typename Value_t>
+    inline Value_t fp_fmms(const Value_t& x, const Value_t& y,
+                           const Value_t& a, const Value_t& b)
+    {
+        return x*y-a*b;
+    }
+
+#ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fmms(const MpfrFloat& x, const MpfrFloat& y,
+                             const MpfrFloat& a, const MpfrFloat& b)
+    {
+        return MpfrFloat::fmms(x,y, a,b);
+    }
+#endif
+
+    // long, mpfr and complex use the default implementation.

extrasrc/functions/func_fms.hh
@@ -1,0 +1,16 @@
+    template<typename Value_t>
+    inline Value_t fp_fms(const Value_t& x, const Value_t& y,
+                          const Value_t& a)
+    {
+        return x*y-a;
+    }
+
+#ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
+    inline MpfrFloat fp_fms(const MpfrFloat& x, const MpfrFloat& y,
+                            const MpfrFloat& a)
+    {
+        return MpfrFloat::fms(x,y, a);
+    }
+#endif
+
+    // long, mpfr and complex use the default implementation.

commit 7f2797f16ae4e84cda5199b5a941f0e7bb2dfc51

Fix more build problems (regression from b837f3b29251910a9ffb68cdf3e8dc671a2e4f87)

fpoptimizer/bytecodesynth.cc
@@ -659,6 +659,12 @@ namespace FPoptimizer_ByteCode
         }
     }
 }
+#undef mData
+#undef mImmed
+#undef mByteCode
+#undef mStackPtr
+#undef mStackSize
+#undef incStackPtr
 
 /* BEGIN_EXPLICIT_INSTANTATION */
 #include "instantiate.hh"